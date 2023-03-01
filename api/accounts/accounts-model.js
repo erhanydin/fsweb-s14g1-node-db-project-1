3bf41adac78785a9a4f76005f0aa4deac7027287
erhanaydin <erhnaydn1999@gmail.com>
2023-03-01 06:34:05 +0300

    Done!

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,36 @@
+const db = require("../../data/db-config");
+
+
 const getAll = () => {
   // KODLAR BURAYA
+  const accounts = db.select("*").from('accounts');
+  return accounts;
 }
 
 const getById = id => {
   // KODLAR BURAYA
+  const accountById = db('accounts').where('id', id);
+  return accountById;
 }
 
 const create = account => {
   // KODLAR BURAYA
+  const insertedAccount = db("accounts").insert(account).then(id => {
+    return getById(id);
+  });
+  return insertedAccount;
 }
 
 const updateById = (id, account) => {
   // KODLAR BURAYA
+  return db("accounts").where("id", id).update(account).then(record => {
+    return getById(id);
+  })
 }
 
 const deleteById = id => {
   // KODLAR BURAYA
+  return db("accounts").where("id", id).del();
 }
 
 module.exports = {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,69 @@
 const router = require('express').Router()
+const accountsModel = require("./accounts-model");
+const middleware = require("./accounts-middleware");
 
-router.get('/', (req, res, next) => {
+
+router.get('/', async (req, res, next) => {
   // KODLAR BURAYA
+  try {
+    const allAccounts = await accountsModel.getAll();    
+    res.status(200).json(allAccounts);
+  } catch (error) {
+    next(error);
+  }
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', middleware.checkAccountId ,async (req, res, next) => {
   // KODLAR BURAYA
+  try {
+    let existedAccount = req.Account;
+    res.json(existedAccount)
+  } catch (error) {
+    next(error);
+  }
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', middleware.checkAccountPayload, middleware.checkAccountNameUnique ,async (req, res, next) => {
   // KODLAR BURAYA
+  try {
+    let insertedAccount = await accountsModel.create(req.body);
+    res.status(201).json(insertedAccount);
+  } catch (error) {
+    next(error ); 
+  }
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', middleware.checkAccountPayload, middleware.checkAccountId, middleware.checkAccountNameUnique ,async (req, res, next) => {
   // KODLAR BURAYA
+  try {
+    let updatedAccount = await accountsModel.updateById(req.params.id, req.body);
+    res.json(updatedAccount);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', middleware.checkAccountId ,async (req, res, next) => {
   // KODLAR BURAYA
+  try {
+    await accountsModel.deleteById(req.params.id);
+    res.json(req.Account);
+  } catch (error) {
+    next(error);
+  }
+
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // KODLAR BURAYA
 })
+
+
+router.use((err, res, req) => {
+  res.status(err.status || 400).json({
+    customMessage: "Bir haata oluştu",
+    message: err.message
+  });
+});
 
 module.exports = router;
